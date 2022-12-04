472bcf520fcc51820364f976762457f05f2cc4f3
Harry Woollen <HarryJamesWoollen@Gmail.Com>
2022-12-04 18:22:22 +0000

    create dict solution
    Updated the old solution to use a dictionary

--- aoc_2022_12_02.py
+++ aoc_2022_12_02.py
@@ -11,54 +11,38 @@ import numpy as np
 with open ('aoc_2_data.txt') as f:
     text = f.read()
 
-
+#Define rock paper scissors function
 def rock_paper_scissors(round_tuple, player_1, player_2):
     """
     A game of rock paper scissors where you input each round as a tuple (e.g. ['Rock', 'Paper'])
     Outputs the scores from each round based on the players inputs.
     """
+    score_dict = {
+    'Rock' : 1,
+    'Paper' : 2,
+    'Scissors' : 3,
+    }
+
+    loses_to = {
+    "Rock": "Paper",
+    "Paper": "Scissors",
+    "Scissors": "Rock",
+    }
+    
     for i in round_tuple:
 #Add scores for each input
-        if i[0] == 'Rock':
-            player_1.append(1)
-        elif i[0] == 'Paper':
-            player_1.append(2)
-        elif i[0] == 'Scissors':
-            player_1.append(3)
-
-        if i[1] == 'Rock':
-            player_2.append(1)
-        elif i[1] == 'Paper':
-            player_2.append(2)
-        elif i[1] == 'Scissors':
-            player_2.append(3)
-
+        player_1.append(score_dict[i[0]])
+        player_2.append(score_dict[i[1]])
+        
 #Add scores for outcome
-#Draw
-        if i[0] == i[1]:
+        if i[1] == loses_to[i[0]]:
+            player_2.append(6)
+
+        elif i[0] == loses_to[i[1]]:
+            player_1.append(6)
+        else:
             player_1.append(3)
             player_2.append(3)
-
-    #Player 1 plays rock
-        elif i[0] == 'Rock':
-            if i[1] == 'Scissors':
-                player_1.append(6)
-            else:
-                player_2.append(6)
-
-    #Player 1 plays scissors
-        elif i[0] == 'Scissors':
-            if i[1] == 'Paper':
-                player_1.append(6)
-            else:
-                player_2.append(6)
-
-    #Player 1 plays paper
-        elif i[0] == 'Paper':
-            if i[1] == 'Rock':
-                player_1.append(6)
-            else:
-                player_2.append(6)
 
     return(player_1, player_2)
 
@@ -72,36 +56,24 @@ round_split = (np.char.split(np.array(line_sep),sep='\n'))
 #Create the object as an array
 round_split = (np.array(round_split[0]))
 
-# Replace letters with 'Rock', 'Paper', 'Scissors'
-# Mainly wanted player 1 and 2 to have the same coding to use ==. 
+
+## For Part 1: 
+
+# Replace all letters with 'Rock', 'Paper', 'Scissors'
 round_split_replace = ([s.replace("A","Rock").replace("B", "Paper").replace("C", "Scissors").replace("X","Rock").replace("Y", "Paper").replace("Z", "Scissors") for s in round_split])
 #Seperate each element
 round_tuple = (np.char.split(round_split_replace, sep = ' '))
 
 
-# Could do one pass through to determine the score for each player based on what they played
-# And then a second passthrough based on if they won. 
-
-player_1 = []
-player_2 = []
-
-player_1, player_2 = rock_paper_scissors(round_tuple=round_tuple, player_1=player_1, player_2=player_2)
-
-print("Players 1 score is:", sum(player_1), "My score is:", sum(player_2))
-
-
-
 ### Pt 2 : X, Y , Z now means Lose, Draw, Win. 
 
-
-
-# Replace all Ys with the same element
-
+#Only rename A, B and C
 round_split_pt2_replace = ([s.replace("A","Rock").replace("B", "Paper").replace("C", "Scissors") for s in round_split])
 round_tuple_pt2 = (np.char.split(round_split_pt2_replace, sep = ' '))
 
 #Code to map players twos input 'X', 'Y', 'Z' to lose, draw, win. 
 for i in round_tuple_pt2:
+    # Replace all Ys with the same element
     if i[1] == 'Y':
         i[1] = i[0]
 
@@ -124,6 +96,15 @@ for i in round_tuple_pt2:
 player_1 = []
 player_2 = []
 
+#Run part 1
+player_1, player_2 = rock_paper_scissors(round_tuple=round_tuple, player_1=player_1, player_2=player_2)
+
+print("For Part 1, Players 1 score is:", sum(player_1), ", My score is:", sum(player_2))
+
+#Run part 2
+player_1 = []
+player_2 = []
+
 player_1, player_2 = rock_paper_scissors(round_tuple=round_tuple_pt2, player_1=player_1, player_2=player_2)
 
-print("Players 1 score is:", sum(player_1), "My score is:", sum(player_2))
+print("For Part 2, Players 1 score is:", sum(player_1), ", My score is:", sum(player_2))
